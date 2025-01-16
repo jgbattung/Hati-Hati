5d6c76b0f711646dde582edcad9904dff77cdc15
Jireh Battung <jirehgbattung@gmail.com>
2025-01-16 18:40:14 +0800

    Protect all routes except landing and auth pages

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,6 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher(['/groups(.*)'])
+const isProtectedRoute = createRouteMatcher(['/', '/sign-in(.*)', '/sign-up(.*)'])
 
 export default clerkMiddleware(async (auth, req) => {
     if (isProtectedRoute(req)) await auth.protect();
