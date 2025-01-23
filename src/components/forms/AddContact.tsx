3d5b4651392e956fed05bcf9d1c41448096e1c2d
Jireh Battung <jirehgbattung@gmail.com>
2025-01-23 20:55:44 +0800

    Add form

--- src/components/forms/AddContact.tsx
+++ src/components/forms/AddContact.tsx
@@ -1,11 +1,27 @@
 "use client"
 
+import { useForm } from "react-hook-form"
 import { Button } from "../ui/button"
-import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "../ui/dialog"
+import { zodResolver } from '@hookform/resolvers/zod'
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from "../ui/dialog"
 import { Input } from "../ui/input"
-import { Label } from "../ui/label"
+import { z } from "zod";
+import { ContactValidation } from "@/lib/validations/contact"
+import { Form, FormControl, FormField, FormItem, FormLabel } from "../ui/form"
 
 const AddContact = () => {
+  const form = useForm<z.infer<typeof ContactValidation>>({
+    resolver: zodResolver(ContactValidation),
+    defaultValues: {
+      name: "",
+      email: "",
+    },
+  })
+
+  const onSubmit = async () => {
+    console.log("SUBMIT")
+  }
+
   return (
     <Dialog>
       <DialogTrigger asChild>
@@ -18,32 +34,50 @@ const AddContact = () => {
             {`Add a new contact to Hati-hati and add them to your groups.`}
           </DialogDescription>
         </DialogHeader>
-        <div className='grid gap-4 py-4'>
-          <div className='grid grid-cols-4 items-center gap-4'>
-            <Label htmlFor="name" className='text-right'>Name</Label>
-            <Input
-              id="name"
-              className='col-span-3'
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)}>
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem className="grid grid-cols-4 items-center gap-4">
+                  <FormLabel className="text-right">Name</FormLabel>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      className="col-span-3"
+                    />
+                  </FormControl>
+                </FormItem>
+              )}
             />
-          </div>
-        </div>
-        <div className='grid gap-4 py-4'>
-          <div className='grid grid-cols-4 items-center gap-4'>
-            <Label htmlFor="email" className='text-right'>Email</Label>
-            <Input
-              id="email"
-              className='col-span-3'
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem className="grid grid-cols-4 items-center gap-4">
+                  <FormLabel className="text-right">Email</FormLabel>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      className="col-span-3"
+                    />
+                  </FormControl>
+                </FormItem>
+              )}
             />
-          </div>
-        </div>
-        <DialogFooter className='flex flex-col gap-2'>
-          <Button>
-            Cancel
-          </Button>
-          <Button>
-            Add new contact
-          </Button>
-        </DialogFooter> 
+            <div className="flex flex-col gap-4">
+              <Button
+                type="submit"
+              >
+                Cancel
+              </Button>
+              <Button>
+                Add new contact
+              </Button>
+            </div>
+          </form>
+        </Form>
       </DialogContent>
     </Dialog>
   )
