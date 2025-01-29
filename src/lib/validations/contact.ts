fabe49a0f3e9bea3237689512d12cd4f3005f822
Jireh Battung <jirehgbattung@gmail.com>
2025-01-29 18:58:19 +0800

    update email validation

--- src/lib/validations/contact.ts
+++ src/lib/validations/contact.ts
@@ -4,5 +4,7 @@ export const ContactValidation = z.object({
   name: z.string()
     .min(1, { message: 'Name is required.' })
     .max(30, { message: 'Name must be 30 characters or fewer.' }),
-  email: z.string().min(1, { message: 'Email is required.' })
+  email: z.string()
+    .min(1, { message: 'Email is required.' })
+    .email({ message: 'Please enter a valid email address' })
 });
