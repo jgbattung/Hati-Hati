44be324190c4a899b65a955f744d77565c6bbdbe
Jireh Battung <jirehgbattung@gmail.com>
2025-02-06 18:16:27 +0800

    Handle no friends

--- src/app/(root)/friends/page.tsx
+++ src/app/(root)/friends/page.tsx
@@ -13,26 +13,32 @@ const Friends = async () => {
   return (
     <div className='flex flex-col items-center justify-center gap-4'>
       <div className='w-full px-5 mt-10'>
-        {userFriends?.map((friends) => (
-          <div
-            key={friends.friend.id}
-            className='flex items-center justify-between my-3'
-          >
-            <div className='flex items-center justify-center gap-4'>
-              <Image
-                src={friends.friend.image!}
-                alt={`${friends.friend.name}'s profile picture`}
-                width={28}
-                height={28}
-                className='rounded-full'
-              />
-              <p>{friends.friend.name}</p>
-            </div>
-            <div>
-              <p className='text-xs font-light text-zinc-400'>no expenses</p>
-            </div>
+        {!userFriends ? (
+          <div className='flex text-zinc-400 items-center justify-center my-10'>
+            <p>You have no friends yet.</p>
           </div>
-        ))}
+        ) : (
+          userFriends?.map((friends) => (
+            <div
+              key={friends.friend.id}
+              className='flex items-center justify-between my-3'
+            >
+              <div className='flex items-center justify-center gap-4'>
+                <Image
+                  src={friends.friend.image!}
+                  alt={`${friends.friend.name}'s profile picture`}
+                  width={28}
+                  height={28}
+                  className='rounded-full'
+                />
+                <p>{friends.friend.name}</p>
+              </div>
+              <div>
+                <p className='text-xs font-light text-zinc-400'>no expenses</p>
+              </div>
+            </div>
+          ))
+        )}
       </div>
       <AddContact />
     </div>
