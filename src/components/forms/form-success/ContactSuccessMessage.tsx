82854c6690dd1d4cdcc8449a6fe077e54358447f
Jireh Battung <jirehgbattung@gmail.com>
2025-02-06 17:08:26 +0800

    Update colors and icons

--- src/components/forms/form-success/ContactSuccessMessage.tsx
+++ src/components/forms/form-success/ContactSuccessMessage.tsx
@@ -25,7 +25,7 @@ const ContactSuccessMessage = ({ message, onClose }: ContactSuccessMessageProps)
         <p className="text-center text-zinc-400 text-sm">{message}</p>
         <Button 
           onClick={onClose}
-          className="px-4 py-2 rounded-md max-md:w-full bg-emerald-600 hover:bg-emerald-700 text-zinc-100 transition-colors"
+          className="px-4 py-2 rounded-md max-md:w-full bg-teal-600 hover:bg-teal-700 text-zinc-100 transition-colors"
         >
           Continue
         </Button>
