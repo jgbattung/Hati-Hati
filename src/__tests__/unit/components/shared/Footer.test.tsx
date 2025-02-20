fdab05ae4c86bd4c5fa2f77760299e703b67632a
Jireh Battung <jirehgbattung@gmail.com>
2025-02-20 15:42:18 +0800

    Add tests

--- src/__tests__/unit/components/shared/Footer.test.tsx
+++ src/__tests__/unit/components/shared/Footer.test.tsx
@@ -1,6 +1,7 @@
 import Footer from "@/components/shared/Footer";
 import { footerRoutes } from "@/constants";
 import { footerTestIds } from "@/utils/constants";
+import { useAuth } from "@clerk/nextjs";
 import { render, screen } from "@testing-library/react"
 
 
@@ -8,12 +9,20 @@ const renderFooter = () => {
   render(<Footer />);
 };
 
+jest.mock("@clerk/nextjs", () => ({
+  useAuth: jest.fn()
+}));
+
 jest.mock('next/navigation', () => ({
-  usePathname: () => '/',
+  usePathname: () => '/groups',
 }));
 
 describe("Footer tests", () => {
   it("should display the Footer without crashing", () => {
+    (useAuth as jest.Mock).mockReturnValue({
+      isSignedIn: true,
+    });
+
     renderFooter();
 
     const footer = screen.getByTestId(footerTestIds.footer);
@@ -21,11 +30,26 @@ describe("Footer tests", () => {
   });
 
   it('should display the correct icons', () => {
+    (useAuth as jest.Mock).mockReturnValue({
+      isSignedIn: true,
+    });
+
     renderFooter();
 
     footerRoutes.forEach((route) => {
       const icon = screen.getByTestId(`icon-${route.iconId}`);
       expect(icon).toBeInTheDocument();
     });
+  });
+
+  it('should not render when user is not signed in', () => {
+    (useAuth as jest.Mock).mockReturnValue({
+      isSignedIn: false,
+    });
+
+    const { container } = render(<Footer />);
+
+    expect(screen.queryByTestId(footerTestIds.footer)).not.toBeInTheDocument();
+    expect(container.firstChild).toBeNull();
   });
 });
