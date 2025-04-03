df4e3206f7de7ddab39876c544405b1212e1b34c
Jireh Battung <jirehgbattung@gmail.com>
2025-04-03 16:52:36 +0800

    Conditionally render settings

--- src/components/shared/Header.tsx
+++ src/components/shared/Header.tsx
@@ -6,9 +6,15 @@ import Link from 'next/link'
 import { usePathname, useRouter } from 'next/navigation'
 import React from 'react'
 
-const Header = () => {
+interface HeaderProps {
+  headerTitle?: string;
+}
+
+const Header = ({ headerTitle }: HeaderProps) => {
   const pathname = usePathname();
   const router = useRouter();
+
+  const isSettingsPage = pathname.endsWith('/settings');
 
   return (
     <section
@@ -16,20 +22,27 @@ const Header = () => {
       className='fixed top-0 w-full bg-transparent z-50 py-4 h-14'
     >
       <div className='flex items-center justify-between px-4'>
-        <button
-          data-testid={headerTestIds.backButton}
-          onClick={() => router.back()}
-          className='hover:text-zinc-300 transition-colors'
-        >
-          <ArrowLeft size={22} />
-        </button>
-        <Link
-          data-testid={headerTestIds.settingsButton}
-          href={`${pathname}/settings`}
-          className='hover:text-zinc-300 transition-colors'
-        >
-          <Settings size={22} />
-        </Link>
+        <div className='flex items-center justify-center gap-3'>
+          <button
+            data-testid={headerTestIds.backButton}
+            onClick={() => router.back()}
+            className='hover:text-zinc-300 transition-colors'
+          >
+            <ArrowLeft size={22} />
+          </button>
+          {isSettingsPage && headerTitle && (
+            <p className='font-light text-lg'>{headerTitle}</p>
+          )}
+        </div>
+        {!isSettingsPage && (
+          <Link
+            data-testid={headerTestIds.settingsButton}
+            href={`${pathname}/settings`}
+            className='hover:text-zinc-300 transition-colors'
+          >
+            <Settings size={22} />
+          </Link>
+        )}
       </div>
     </section>
   )
